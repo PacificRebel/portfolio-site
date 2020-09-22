fc78d665a71746491472fa9ee7bbf80b2537b99a
Heli Sivunen <heli@mooncat.tv>
2020-09-22 13:42:35 +0100

    Implement flexbox on first two components.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import GitHubCall from './GitHubCall';
 import RepoLink from './RepoLink';
 import './App.css';
+import { Column, Row } from 'simple-flexbox';
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <GitHubCall />
-        <RepoLink />
-      </div>
+      <Column flexGrow={1}>
+        <Row horizontal='center'>
+          <h1><GitHubCall /></h1>
+        </Row>
+        <Row vertical='center'>
+          <Column flexGrow={1} horizontal='center'>
+            <h3> Contact </h3>
+            <span>  </span>
+          </Column>
+          <Column flexGrow={1} horizontal='center'>
+            <span> <RepoLink /></span>
+          </Column>
+        </Row>
+      </Column>
     );
   }
 }
