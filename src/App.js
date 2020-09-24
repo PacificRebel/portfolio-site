fdb71a7eb21dfb5cd3a9d73d324a31a3a2fac29b
Heli Sivunen <heli@mooncat.tv>
2020-09-24 16:11:12 +0100

    Add notes in app js to make sense of grid

--- src/App.js
+++ src/App.js
@@ -9,40 +9,53 @@ import { Column, Row } from 'simple-flexbox';
 class App extends Component{
   render(){
     return(
+// start of main column (whole page?)
       <Column flexGrow={1}>
 
-        <Row horizontal='space-between'>
-        <Column flexGrow={1} horizontal='left'>
-          <span> <GitHubCall />  </span>
-        </Column>
-        <Column flexGrow={1} horizontal='left'>
-          <span>    </span>
-        </Column>
-        <Column
-                flexGrow={1}
-                horizontal='left'
-                style={{ fontFamily: 'Special Elite' }}>
-          <span> /about  </span>
-        </Column>
-        </Row>
-
+// first row starts here
         <Row vertical='center'>
 
-          <Column flexGrow={1} horizontal='center'>
-            <span> <RepoLink />  </span>
-          </Column>
+// first column in first row (my name)
+        <Column flexGrow={1} horizontal='center'>
+          <span><GitHubCall /></span>
+        </Column>
 
-          <Column flexGrow={1} horizontal='center'>
-            <span> <BlogLink /> </span>
-          </Column>
+// second column in first row (empty space in between)
+        <Column flexGrow={1} horizontal='center'>
+          <span></span>
+        </Column>
 
-          <Column flexGrow={1} horizontal='center' 
-          style={{ fontFamily: 'Special Elite' }}>
-            <span> <StarredRepos /> </span>
-          </Column>
-
+// third column in first row (this will be link to bio)
+        <Column
+                flexGrow={1}
+                horizontal='center'
+                style={{ fontFamily: 'Special Elite' }}>
+          <span>/about</span>
+        </Column>
+// end of first row
         </Row>
 
+// second row starts here
+        <Row vertical='center'>
+
+// first column of second row (link to github repos - not needed)
+          <Column flexGrow={1} horizontal='center'>
+            <span><RepoLink /></span>
+          </Column>
+
+// second column of second row (link to blog)
+          <Column flexGrow={1} horizontal='center'>
+            <span><BlogLink /></span>
+          </Column>
+
+// third column of second row (starred repos - should be just one repo)
+          <Column flexGrow={1} horizontal='center'
+          style={{ fontFamily: 'Special Elite' }}>
+            <span><StarredRepos /></span>
+          </Column>
+// end of second row
+        </Row>
+// end of main column (whole page?)
       </Column>
     );
   }
